ebe0b82eb11635d54bb23837185c0c0bf9f672fb
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-03-05 15:12:57 -0300

    Trying to understand JavaFX

--- src/br/eng/crisjr/failproof/desktop/Controller.java
+++ src/br/eng/crisjr/failproof/desktop/Controller.java
@@ -1,4 +1,22 @@
 package br.eng.crisjr.failproof.desktop;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML private StackPane stackTitles;
+
+    public Controller() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        stackTitles.getChildren().add(0, new Label("let's see"));
+    }
 }
