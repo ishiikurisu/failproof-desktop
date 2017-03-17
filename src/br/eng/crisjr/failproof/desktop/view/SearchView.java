69e953b38952e3cda476574d2780e7c8e3065858
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-03-17 14:56:35 -0300

    Making window keep existing after closing

--- src/br/eng/crisjr/failproof/desktop/view/SearchView.java
+++ src/br/eng/crisjr/failproof/desktop/view/SearchView.java
@@ -1,24 +1,21 @@
 package br.eng.crisjr.failproof.desktop.view;
 
 import javax.swing.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import br.eng.crisjr.failproof.desktop.controller.SearchController;
 
-public class SearchView extends JFrame {
+public class SearchView extends JFrame implements WindowListener {
     private SearchController controller = null;
     private JLabel[] titles = new JLabel[] { new JLabel("Downloading...") };
+    private JPanel panelTitles = new JPanel();
 
     public SearchView() {
         // Building initial window
-        JPanel grid = new JPanel();
-        for (int i = 0; i < titles.length; i++) {
-            grid.add(titles[i]);
-        }
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitles(titles);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.addWindowListener(this);
         this.setSize(600, 400);
-		this.add(grid);
-        this.add(grid);
+		this.add(panelTitles);
     }
 
     public void setController(SearchController controller) {
@@ -31,4 +28,27 @@ public class SearchView extends JFrame {
     public void windowClosing(WindowEvent event) {
         controller.showParent();
     }
+
+    public void setTitles(String[] stuff) {
+        int limit = stuff.length;
+        titles = new JLabel[limit];
+        for (int i = 0; i < limit; ++i) {
+            titles[i] = new JLabel(stuff[i]);
+        }
+    }
+
+    public void setTitles(JLabel[] labels) {
+        panelTitles.removeAll();
+        for (int i = 0; i < labels.length; ++i) {
+            panelTitles.add(labels[i]);
+        }
+    }
+
+    /* WINDOW EVENTS */
+    public void windowActivated(WindowEvent e) { }
+    public void windowClosed(WindowEvent e) { }
+    public void windowDeactivated(WindowEvent e) { }
+    public void windowDeiconified(WindowEvent e) { }
+    public void windowIconified(WindowEvent e) { }
+    public void windowOpened(WindowEvent e) { }
 }
